f45708cd497d44eb27aa9d8cdcf6eff526eb1c44
Killian Nallet <194366242+nalkn@users.noreply.github.com>
2026-02-24 18:50:04 +0100

    Fix DNS redirection for captive portal

--- src/bin/server-check_config.sh
+++ src/bin/server-check_config.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-FILE="/etc/dnsmasq.d/090_wlan0.conf"
-CHECKLINE="address=/#/10.3.141.1"
-
 # check config
-if ! grep -Fxq "$CHECKLINE" "$FILE"; then
+
+FILE="/etc/dnsmasq.d/090_wlan0.conf"
+CHECK_LINE="address=/#/10.3.141.1"
+DEL_LINE="dhcp-option=6,9.9.9.9,1.1.1.1"
+
+# add line
+if ! grep -Fxq "$CHECK_LINE" "$FILE"; then
     echo "[INFO] Adjust dnmasq config"
     cat >> "/etc/dnsmasq.d/090_wlan0.conf" <<- 'EOF'
 
 # Redirect DNS to the Pi (captive portal detection)
 address=/#/10.3.141.1
-
-# Captive Portal Hint (Android / modern OS)
-#dhcp-option=114,http://10.3.141.1/
 EOF
-
-    # restart dnsmasq
-    systemctl restart dnsmasq
 fi
+
+# del line
+if grep -Fxq "$DEL_LINE" "$FILE"; then
+    echo "[INFO] Removing external DNS option"
+    sed -i "\|^$DEL_LINE$|d" "$FILE"
+fi
+
+# restart dnsmasq
+systemctl restart dnsmasq
